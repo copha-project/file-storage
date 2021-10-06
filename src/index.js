56d317bd913da73899d12b398efc320d2ac01251
aong <yxwzaxns@gmail.com>
2021-10-07 01:05:11 +0800

    Initial commit

---
+++ src/index.js
@@ -0,0 +1,40 @@
+const path = require('path')
+const { Storage, Utils } = require('copha')
+
+class File extends Storage {
+    constructor(taskConf) {
+        super(taskConf)
+    }
+
+    #getDetailPath(id){
+        const filename = `${id}.json`
+        return path.join(this.taskConf.main.dataPath,'detail', filename)
+    }
+
+    #getPath(name){
+        const pathList = {
+            saveDetailDataDir: path.join(this.taskConf.main.dataPath,'detail')
+        }
+        return pathList[name]
+    }
+
+    async init(){}
+
+    async findById(id){
+        const isExist = await Utils.checkFile(this.#getDetailPath(id))
+        return isExist ? Utils.readJson(this.#getDetailPath(id)) : null
+    }
+
+    async all(){
+        return (await Utils.readDir(this.#getPath('saveDetailDataDir')))
+            .filter(f => f.endsWith('.json'))
+            .map(f => path.join(this.#getPath('saveDetailDataDir'), f))
+    }
+    
+    async save({data,id}){
+        await Utils.saveJson(data, this.#getDetailPath(id))
+    }
+
+    async close(){}
+}
+module.exports = File
